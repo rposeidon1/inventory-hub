5bd78532d4f2634b1c614520f1c0c0ded99b44f6
Ashfall <preshiouzemerad@gmail.com>
2026-06-25 15:19:36 +0100

    explicit LOGIN_URL and resolve home redirect loop

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,6 +131,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-
+LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
